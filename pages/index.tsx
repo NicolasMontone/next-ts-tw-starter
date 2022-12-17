d60852f462a047b5edf7857e849037ea1c0f8079
NicolasMontone <montonenicolas01@gmail.com>
2022-12-17 18:16:40 -0300

    fix: apply Page element to index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { Page } from '../components/Page'
+
 export default function Home() {
   return (
     <>
-      <main className="flex justify-center">
-        <h1 className="m-12 text-3xl font-bold">Your code starts here 🚀</h1>
-      </main>
+      <Page>
+        <main className="flex justify-center">
+          <h1 className="m-12 text-3xl font-bold">Your code starts here 🚀</h1>
+        </main>
+      </Page>
     </>
   )
 }
